948f99d69ea189190a383b1515b4fce9b63eb50e
udayrajsawhney <udayraj.s16@iiits.in>
2018-10-07 22:33:15 +0530

    next adding review

---
+++ MoviesDB/app/src/main/java/com/udaysawhney/moviesdb/api/Service.java
@@ -0,0 +1,21 @@
+package com.udaysawhney.moviesdb.api;
+
+import com.udaysawhney.moviesdb.model.MoviesResponse;
+import com.udaysawhney.moviesdb.model.TrailerResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface Service {
+
+    @GET("movie/popular")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{movie_id}/videos")
+    Call<TrailerResponse> getMovieTrailer(@Path("movie_id") int id, @Query("api_key") String apiKey);
+}
